c4f857d49a75df8827ef32de93eb0526c8e50ecb
Marcel Freiberg <marcel.freiberg@outlook.com>
2019-07-28 02:36:34 +0200

    Adding a routine to read the request

--- server.c
+++ server.c
@@ -6,6 +6,10 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
+int listenfd;
+int connfd;
 
 void _initialize_server(uint16_t port)
 {
@@ -15,7 +19,6 @@ void _initialize_server(uint16_t port)
     connection_socket_addr.sin_port = htons(port);
     connection_socket_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    int listenfd;
     if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
         log_error("Could not create listen socket");
@@ -30,8 +33,76 @@ void _initialize_server(uint16_t port)
 void run_server(uint16_t port)
 {
     _initialize_server(port);
+
+    if (listen(listenfd, 10) == -1)
+    {
+        log_error("Could not start listen on socket");
+    }
+
+    while (1)
+    {
+        struct sockaddr client_addr;
+        socklen_t client_addr_len;
+        if ((connfd = accept(listenfd, &client_addr, &client_addr_len)) == -1)
+        {
+            log_error("Could not accept an incomming connection");
+        }
+
+        switch (fork())
+        {
+        case -1: // Error while forking
+            log_error("Could not create a child process for a incomming connection");
+            break;
+
+        case 0: // Child process
+        {
+            char buf[1024];
+            int len;
+
+            while (len > 0 && strcmp("\n", buf))
+            {
+                len = get_line(connfd, buf, sizeof(buf));
+                log_error(buf);
+            }
+
+            break;
+        }
+
+        default:
+            break;
+        }
+    }
 }
 
 void stop_server()
 {
+}
+
+int get_line(int sock, char* buf, int size)
+{
+    int i = 0;
+    char c = '\0';
+    int n;
+
+    while ((i < size - 1) && (c != '\n'))
+    {
+        n = recv(sock, &c, 1, 0);
+        if (n > 0)
+        {
+            if (c == '\r')
+            {
+                n = recv(sock, &c, 1, MSG_PEEK);
+                if ((n > 0) && (c == '\n'))
+                    recv(sock, &c, 1, 0);
+                else
+                    c = '\n';
+            }
+            buf[i] = c;
+            i++;
+        }
+        else
+            c = '\n';
+    }
+    buf[i] = '\0';
+    return i;
 }
--- server.h
+++ server.h
@@ -7,3 +7,5 @@ void _initialize_server(uint16_t port);
 void run_server(uint16_t port);
 
 void stop_server();
+
+int get_line(int sock, char* buf, int size);
