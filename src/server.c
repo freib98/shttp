3c515e244ac2a9ff35de5afff7dda2f8580f6e10
Marcel Freiberg <marcel.freiberg@outlook.com>
2019-08-11 15:17:24 +0200

    Basic functionality to read the request and sending a response

--- src/server.c
+++ src/server.c
@@ -12,35 +12,6 @@ int listenfd;
 int connfd;
 
 // Method Description:
-// - Creating and configurating the socket
-// Arguments:
-// - port: the port to configure the socket
-int _initialize_server(uint16_t port)
-{
-    struct sockaddr_in connection_socket_addr;
-    memset(&connection_socket_addr, 0, sizeof(connection_socket_addr));
-    connection_socket_addr.sin_family = AF_INET;
-    connection_socket_addr.sin_port = htons(port);
-    connection_socket_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    log_debug("Creating the listening socket");
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        log_error("Could not create listen socket");
-        return SERVER_ERROR;
-    }
-
-    log_debug("Binding the listening socket to the port");
-    if (bind(listenfd, (struct sockaddr*)&connection_socket_addr, sizeof(connection_socket_addr)) == -1)
-    {
-        log_error("Could not bind socket to port");
-        return SERVER_ERROR;
-    }
-
-    return SERVER_SUCCESS;
-}
-
-// Method Description:
 // - This is the main routine of the server. It initializes the
 //   socket, listens for connections, accepts them and sends
 //   a response
@@ -48,7 +19,7 @@ int _initialize_server(uint16_t port)
 // - port: the port the socket is listening on
 int run_server(uint16_t port)
 {
-    if (_initialize_server(port) == SERVER_ERROR)
+    if (initialize_server(port) == SERVER_ERROR)
     {
         return SERVER_ERROR;
     }
@@ -78,20 +49,30 @@ int run_server(uint16_t port)
 
         case 0: // Child process
         {
-            char buf[1024];
-            int len;
-
-            do
-            {
-                len = get_line(connfd, buf, sizeof(buf));
-                // printf("%s", buf);
-                log_debug(buf);
-            } while (len > 0 && strcmp("\n", buf));
+            struct request request;
+            receive_request(&request);
 
             char response[1024];
-            strcpy(response, "HTTP/1.0 200 OK\r\nServer: shttp\r\nContent-Type: text/html\r\n\r\nHello World!");
+
+            if (request.http_major_version == 1 && request.http_minor_version == 0)
+            {
+                if (request.method == GET)
+                {
+                    strcpy(response, "HTTP/1.0 200 OK\r\nServer: shttp\r\nContent-Type: text/html\r\n\r\nHello World!");
+                }
+                else
+                {
+                    strcpy(response, "HTTP/1.0 501 Not Implemented\r\nServer: shttp\r\nContent-Type: text/html\r\n\r\nThe method is not implemented");
+                }
+            }
+            else
+            {
+                strcpy(response, "HTTP/1.0 505 HTTP Version not supported\r\nServer: shttp\r\nContent-Type: text/html\r\n\r\nThis http server just allows http 1.0 requests");
+            }
+
             send(connfd, response, strlen(response), 0);
 
+            // Close connection socket
             if (close(connfd) == -1)
             {
                 log_error("Could not close the connection socket");
@@ -106,8 +87,77 @@ int run_server(uint16_t port)
     }
 }
 
+// Method Description:
+// - Creating and configurating the socket
+// Arguments:
+// - port: the port to configure the socket
+int initialize_server(uint16_t port)
+{
+    struct sockaddr_in connection_socket_addr;
+    memset(&connection_socket_addr, 0, sizeof(connection_socket_addr));
+    connection_socket_addr.sin_family = AF_INET;
+    connection_socket_addr.sin_port = htons(port);
+    connection_socket_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    log_debug("Creating the listening socket");
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
+    {
+        log_error("Could not create listen socket");
+        return SERVER_ERROR;
+    }
+
+    log_debug("Binding the listening socket to the port");
+    if (bind(listenfd, (struct sockaddr*)&connection_socket_addr, sizeof(connection_socket_addr)) == -1)
+    {
+        log_error("Could not bind socket to port");
+        return SERVER_ERROR;
+    }
+
+    return SERVER_SUCCESS;
+}
+
 void stop_server()
 {
+}
+
+int receive_request(struct request* request)
+{
+    char buf[1024];
+    size_t buf_len = sizeof(buf);
+    int len;
+
+    len = get_line(connfd, buf, buf_len);
+
+    if (len == 0 || strcmp("\n", buf) == 0)
+    {
+        return SERVER_INVALID_REQUEST;
+    }
+
+    // Method
+    char* token = strtok(buf, " ");
+    if (!strcmp(token, "GET"))
+    {
+        request->method = GET;
+    }
+    else
+    {
+        request->method = UNDEFINED;
+    }
+
+    // URL
+    token = strtok(NULL, " ");
+    strncpy(request->url, token, sizeof(request->url));
+
+    // HTTP VERSION
+    token = strtok(NULL, " ");
+    sscanf(token, "HTTP/%u.%u", &request->http_major_version, &request->http_minor_version);
+
+    // Just read the rest of the request
+    while (len > 0 && strcmp("\n", buf))
+    {
+        len = get_line(connfd, buf, buf_len);
+        log_debug(buf);
+    }
 }
 
 // Method Description:
--- src/server.h
+++ src/server.h
@@ -2,13 +2,29 @@
 
 #include <stdint.h>
 
-enum
+enum server_error
 {
     SERVER_SUCCESS,
-    SERVER_ERROR
+    SERVER_ERROR,
+    SERVER_INVALID_REQUEST
 };
 
-int _initialize_server(uint16_t port);
+enum request_method
+{
+    UNDEFINED,
+    GET
+};
+
+struct request
+{
+    enum request_method method;
+    char url[1024];
+    int http_major_version;
+    int http_minor_version;
+};
+
 int run_server(uint16_t port);
+int initialize_server(uint16_t port);
 void stop_server();
+int receive_request(struct request* request);
 int get_line(int sock, char* buf, int size);
